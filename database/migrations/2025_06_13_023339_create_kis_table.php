b88137d5972472f1acdeefb9ee43e8ade10e3a2c
fitrag <jaya88114@gmail.com>
2025-06-20 07:03:44 +0700

    fix migration kis tabel

--- database/migrations/2025_06_13_023339_create_kis_table.php
+++ database/migrations/2025_06_13_023339_create_kis_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('kis', function (Blueprint $table) {
             $table->id();
-            $able->id();
             $table->string('tp',10);
             $table->date('tgl_pelaksanaan');
             $table->text('deskripsi');
